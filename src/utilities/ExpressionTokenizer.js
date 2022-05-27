ecdd26aab2c826012feb44d5b5fd45fb0ca8d8b9
rurikid <rurikid@tutanota.com>
2022-05-26 22:12:37 -0500

    basic tokenizer

--- src/utilities/ExpressionTokenizer.js
+++ src/utilities/ExpressionTokenizer.js
@@ -2,13 +2,32 @@ class ExpressionTokenizer {
   
   static GetTokens(input) {
     console.log(input);
-    input.split('').forEach(digit => {
 
+    let tokens = [];
+
+    input.split('').forEach(char => {
+      if (this.isLiteral(char)) { tokens.push(ExpressionToken("Literal", char)); }
+      else if (this.isLetter(char)) { tokens.push(ExpressionToken("Letter", char)); } 
+      else if (this.isOperator(char)) { tokens.push(ExpressionToken("Operator", char)); }
+      else if (this.isLeftParenthesis(char)) { tokens.push(ExpressionToken("Left Parenthesis", char)); }
+      else if (this.isRightParenthesis(char)) { tokens.push(ExpressionToken("Right Parenthesis", char)); }
     });
+
+    console.log(tokens);
   }
+
+  static isLiteral = (char) => /\d|_|\./.test(char);
+
+  static isOperator = (char) => /÷|\*|\+|-|\^/.test(char);
+
+  static isLetter = (char) => /[a-z]/i.test(char);
+
+  static isRightParenthesis = (char) => char === ')';
+
+  static isLeftParenthesis = (char) => char === '(';
 }
 
-const ExpressionToken = (type, value) => { this.type = type; this.value = value; }
+const ExpressionToken = (type, value) => ({ type: type, value: value })
 
 const ExpressionErrors = {
   syntaxError: "SYNTAXØERROR",
