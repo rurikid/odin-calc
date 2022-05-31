83e94e85f4b5cd0840f9c1a8f399918884b12324
rurikid <rurikid@tutanota.com>
2022-05-31 10:56:12 -0500

    completed tokenizer and token evaluator

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Keyboard } from "./src/ui/hocs/keyboard.js";
 import { Controls } from "./src/controls.js"; 
 import { Display } from "./src/ui/hocs/display.js";
-import { ExpressionTokenizer } from "./src/utilities/ExpressionTokenizer.js";
 import { VSHeader } from "./src/vs-common/vs-header.js";
 
 document.getElementById("header").appendChild(VSHeader.render());
@@ -11,6 +10,3 @@ let display = new Display();
 canvas.appendChild(display.node);
 let controls = new Controls(display);
 canvas.appendChild(Keyboard.getKeyboard(controls.Keys));
-
-display.input.setDisplay("_2.16*SIN(42)+(64÷3*COS(42))-16^3-INV(32-TAN(64+(_1.42*_8.64)))");
-ExpressionTokenizer.GetTokens(display.input.input);
--- src/utilities/ExpressionTokenizer.js
+++ src/utilities/ExpressionTokenizer.js
@@ -1,5 +1,5 @@
 class ExpressionTokenizer {
-  
+   
   static GetTokens(input) {
     console.log(input);
 
@@ -10,25 +10,25 @@ class ExpressionTokenizer {
 
       if (this.isLiteral(char)) {
         if (token.type === undefined) {
-          token = ExpressionToken('Literal', char);
-        } else if (token.type === 'Letter') {
+          token = ExpressionToken(TokenTypes.literal, char === '_' ? '-' : char);
+        } else if (token.type === TokenTypes.function) {
           tokens.push(token);
-          token = ExpressionToken('Literal', char);
-        } else if (token.type === 'Literal') {
-          token.value += char;
+          token = ExpressionToken(TokenTypes.literal, char === '_' ? '-' : char);
+        } else if (token.type === TokenTypes.literal) {
+          token.value += char === '_' ? '-' : char;
         }
         return;
       }
       else if (this.isLetter(char)) { 
         if (token.type === undefined) {
-          token = ExpressionToken('Letter', char);
-        } else if (token.type === "Letter") {
+          token = ExpressionToken(TokenTypes.function, char);
+        } else if (token.type === TokenTypes.function) {
           token.value += char;
-        } else if (token.type === "Literal") {
+        } else if (token.type === TokenTypes.literal) {
           tokens.push(token);
-          token = ExpressionToken('Literal', char);
+          token = ExpressionToken(TokenTypes.literal, char);
         } else {
-          token = ExpressionToken('Letter', char);
+          token = ExpressionToken(TokenTypes.function, char);
         }
         return;
       }
@@ -38,12 +38,139 @@ class ExpressionTokenizer {
         token = ExpressionToken();
       }
 
-      if (this.isOperator(char)) { tokens.push(ExpressionToken("Operator", char)); }
-      if (this.isLeftParenthesis(char)) { tokens.push(ExpressionToken("Left Parenthesis", char)); }
-      if (this.isRightParenthesis(char)) { tokens.push(ExpressionToken("Right Parenthesis", char)); }
+      if (this.isOperator(char)) { tokens.push(ExpressionToken(TokenTypes.operator, char)); }
+      if (this.isLeftParenthesis(char)) { tokens.push(ExpressionToken(TokenTypes.leftParenthesis, char)); }
+      if (this.isRightParenthesis(char)) { tokens.push(ExpressionToken(TokenTypes.rightParenthesis, char)); }
     });
 
-    console.log(tokens);
+    return tokens;
+  }
+
+  static evaluate(tokens) {
+    // error cases
+    if ((tokens[0].type === TokenTypes.function &&
+      tokens[1].type !== TokenTypes.leftParenthesis) ||
+      tokens[0].type === TokenTypes.operator) throw ExpressionErrors.syntaxError;
+    
+    // recursion end cases
+    if (tokens.length === 0) return ExpressionToken[TokenTypes.literal, 0];
+    if (tokens.length === 1 &&
+      tokens[0].type === TokenTypes.literal) return tokens[0];
+
+    // first element function or group
+    if (tokens[0].type === TokenTypes.rightParenthesis) return ExpressionToken[TokenTypes.literal, 0];
+    if (tokens[0].type === TokenTypes.function) {
+      tokens = this.evaluateFunction(tokens);
+    }
+    if (tokens[0].type === TokenTypes.leftParenthesis) {
+      tokens = this.evaluateGroup(tokens);
+    }
+    
+    // evaluate literal + operator + (literal/function/group);
+    if (tokens[0].type === TokenTypes.literal && tokens[1].type === TokenTypes.operator) {
+      if (tokens.length > 3 && tokens[2].type === TokenTypes.literal &&
+        tokens[3].value === "^") {
+          tokens = [...tokens.slice(0, 2), ...this.operateGroup(tokens.slice(2))];
+        } else {
+          tokens = this.operateGroup(tokens);
+        }
+
+      return this.evaluate(tokens);
+    }
+
+    return tokens[0];
+  }
+
+  static operateGroup(tokens) {
+    // evaluate literal + operator + (literal/function/group);
+    if (tokens[2].type === TokenTypes.literal) {
+      let resultToken = this.operate(tokens[0], tokens[1], tokens[2]);
+      tokens = [resultToken, ...tokens.slice(3)];
+    } else if (tokens[2].type === TokenTypes.function) {
+      let functionResultTokens = this.evaluateGroup(tokens.slice(3));
+      let resultToken = this.operate(tokens[0], tokens[1],
+        this.performFunction(tokens[2].value, functionResultTokens[0]));
+      tokens = [resultToken, ...functionResultTokens.slice(1)];
+    } else if (tokens[2].type === TokenTypes.leftParenthesis) {
+      let groupResultTokens = this.evaluateGroup(tokens.slice(2));
+      let resultToken = this.operate(tokens[0], tokens[1], groupResultTokens[0]);
+      tokens = [resultToken, ...groupResultTokens.slice(1)];
+    }
+    return tokens;
+  }
+
+  static evaluateGroup(tokens) {
+    let closeIndex = this.getClosingGroupIndex(tokens);
+    let groupResultToken = this.evaluate(tokens.slice(1, closeIndex));
+    return [groupResultToken, ...tokens.slice(closeIndex + 1)];
+  }
+
+  static evaluateFunction(tokens) {
+    let closeIndex = this.getClosingGroupIndex(tokens.slice(1));
+    let functionResultToken = this.performFunction(tokens[0].value, tokens.slice(2, closeIndex + 1)); // +1 for sliced function
+    return [functionResultToken, ...tokens.slice(closeIndex + 2)];
+  }
+
+  static getClosingGroupIndex(tokens) {
+    let parenthesisCount = 0;
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (tokens[i].type === TokenTypes.leftParenthesis) parenthesisCount++;
+      if (tokens[i].type === TokenTypes.rightParenthesis) parenthesisCount--;
+      if (parenthesisCount === 0) return i;
+    }
+  }
+
+  static operate(lhs, operator, rhs) {
+    let result;
+    switch(operator.value) {
+      case '^':
+        result = Math.pow(parseFloat(lhs.value), parseFloat(rhs.value));
+        break;
+      case '÷':
+        if (rhs.value === '0') throw ExpressionErrors.divideByZero;
+        result = parseFloat(lhs.value) / parseFloat(rhs.value);
+        break;
+      case '*':
+        result = parseFloat(lhs.value) * parseFloat(rhs.value);
+        break;
+      case '-':
+        result = parseFloat(lhs.value) - parseFloat(rhs.value);
+        break;
+      case '+':
+        result = parseFloat(lhs.value) + parseFloat(rhs.value);
+        break;
+      default:
+        result = parseFloat(lhs.value);
+    }
+    return ExpressionToken(TokenTypes.literal, result);
+  }
+
+  static performFunction(method, token) {
+    let value = parseFloat(token.value);
+    let result;
+    switch (method) {
+      case 'SIN':
+        result = Math.sin(value);
+        break;
+      case 'COS':
+        result = Math.cos(value);
+        break;
+      case 'TAN':
+        result = Math.tan(value);
+        break;
+      case 'INV':
+        if (value === 0) throw ExpressionErrors.divideByZero;
+        result = 1 / value;
+        break;
+      case 'SQRT':
+        if (value < 0) throw ExpressionErrors.sqrtNegative;
+        result = Math.sqrt(value);
+        break;
+      default:
+        result = 0;
+    }
+    return ExpressionToken(TokenTypes.literal, result);
   }
 
   static isLiteral = (char) => /\d|_|\./.test(char);
@@ -62,7 +189,16 @@ const ExpressionToken = (type, value) => ({ type: type, value: value })
 const ExpressionErrors = {
   syntaxError: "SYNTAXØERROR",
   divideByZero: "DIVIDEØBYØZERO",
-  overflow: "OVERFLOW"
+  overflow: "OVERFLOW",
+  sqrtNegative: "SQRT(NEG)"
+}
+
+const TokenTypes = {
+  literal: 'Literal',
+  operator: 'Operator',
+  leftParenthesis: 'Left Parenthesis',
+  rightParenthesis: 'Right Parenthesis',
+  function: 'Function'
 }
 
 export { ExpressionTokenizer, ExpressionToken };
